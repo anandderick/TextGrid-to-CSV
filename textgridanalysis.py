ea2ba60af7c6c1e5c2fd578ee98f71196bb7c14b
Anand Derick <46650447+anandderick@users.noreply.github.com>
2023-12-04 16:31:53 -0800

    Update textgridanalysis.py

--- textgridanalysis.py
+++ textgridanalysis.py
@@ -2,7 +2,7 @@ import textgrid
 import csv
 
 # Path to your TextGrid file
-file_path = '/Users/anandderick/College/CORINA LAB RESEARCH /speech1_chunk0.TextGrid'
+file_path = 'Yourfile/textgridfile.TextGrid'
 
 # Load the TextGrid file
 tg = textgrid.TextGrid.fromFile(file_path)
@@ -13,22 +13,6 @@ print(type(tg))
 for tier in tg.tiers:
     print("Tier name:", tier.name) 
 
-# Now you can access the tiers, intervals, etc.
-#for tier in tg.tiers:
- #   print(tier.name)
-  #  for interval in tier:
-   #     print(interval.minTime, interval.maxTime, interval.mark)
-
-# Initialize the matrix with headers
-#matrix = [["ARPAbet", "Start", "End"]]
-
-# Assuming 'L' is in the first tier, and you want to extract intervals that contain 'L'
-
-#for interval in tg[0]:  # Replace 0 with the index of the tier you want to analyze
- #   if interval.mark == "L":
-  #      matrix.append([interval.mark, interval.minTime, interval.maxTime])
-
-# Find and work with the 'phones' tier
 phones_tier = None
 for tier in tg.tiers:
     if tier.name == "phones":
@@ -43,9 +27,6 @@ if phones_tier is not None:
 else:
     print("The 'phones' tier was not found in the TextGrid file.")
 
-    import textgrid
-
-
 # Initialize an empty list to hold all intervals
 interval_matrix = []
 
@@ -57,6 +38,18 @@ for tier in tg.tiers:
             interval_matrix.append([interval.minTime, interval.maxTime, interval.mark])
 
 # Now 'interval_matrix' contains the data in a format that can be written to a CSV file
+
+counter = 0
+#I want to see how many phones are blank
+for tier in tg.tiers:
+    if tier.name == "phones":
+        for interval in tier:
+            if interval.mark == "":
+                counter=counter+1
+
+
+
+print(counter)
 
 # Specify the CSV file name
 csv_file_path = '/Users/anandderick/College/CORINA LAB RESEARCH /phones_intervals.csv'
